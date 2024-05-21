a3cab9f917ceeebbe303ef7a1979579f7e29b478
GET Academy <terje@kolderup.net>
2024-05-21 09:03:26 +0200

    Initial commit

---
+++ TrafficLightAndEncapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using TrafficLightAndEncapsulation;
+
+var red = true;
+var yellow = false;
+var green = false;
+
+while (true)
+{
+    Console.Clear();
+    TrafficLightConsole.Show(red, yellow, green);
+    if (red && !yellow)
+    {
+        yellow = true;
+    }
+    else if (red)
+    {
+        red = false;
+        yellow = false;
+        green = true;
+    }
+    else if (green)
+    {
+        green = false;
+        yellow = true;
+    }
+    else if (yellow)
+    {
+        yellow = false;
+        red = true;
+    }
+
+    Console.ReadKey(true);
+
+}
